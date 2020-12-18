55e7d3a8db796d83bcb2aa6dbbcfec7f92ee69ca
unknown <gomza_97@mail.ru>
2020-12-18 22:27:42 +0300

    commit

---
+++ MyThreads/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+
+
+namespace MyThreads
+{
+    class Program
+    {
+        public static int copiedFilesCount = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter source directory:");
+            string sourceDirectoryName = Console.ReadLine();
+            Console.WriteLine("Please enter destination directory:");
+            string destinationDirectoryName = Console.ReadLine();
+
+            if (sourceDirectoryName.Equals(destinationDirectoryName))
+                throw new ArgumentException("Source and destination directories cant be equal");
+            if (!Directory.Exists(sourceDirectoryName))
+                throw new ArgumentException("Wrong source directory");
+            if (!Directory.Exists(destinationDirectoryName))
+            {
+                try
+                {
+                    Directory.CreateDirectory(destinationDirectoryName);
+                }
+                catch
+                {
+                    throw new Exception("Cant create and find destination directory");
+                }
+            }
+
+            TaskQueue taskQueue = new TaskQueue(100);
+
+            foreach (var directory in Directory.GetDirectories(sourceDirectoryName, "*", SearchOption.AllDirectories))
+            {
+                string newDirectory = directory.Replace(sourceDirectoryName, destinationDirectoryName);
+                try
+                {
+                    Directory.CreateDirectory(newDirectory);
+                }
+                catch 
+                {
+                    Console.WriteLine("Error to create directory");
+                }
+            }
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            foreach (var file in Directory.GetFiles(sourceDirectoryName, "*.*", SearchOption.AllDirectories))
+            {
+                string newFile = file.Replace(sourceDirectoryName, destinationDirectoryName);
+                taskQueue.EnqueueTask(delegate {
+                    try
+                    {
+                        File.Copy(file, newFile, true);
+                        copiedFilesCount++;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error occured while copying files");
+                    }
+                });
+            }
+            
+            taskQueue.Interrupt();
+            Console.WriteLine("Files copied = " + copiedFilesCount);
+            sw.Stop();
+            Console.WriteLine("Time: " + sw.ElapsedMilliseconds + " ms");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ MyThreads/TaskQueue.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MyThreads
+{
+    class TaskQueue
+    {
+        private object locker = new object();
+        private int threadsCount;
+        private Thread[] threads;
+        private Queue<TaskDelegate> taskQueue = new Queue<TaskDelegate>();
+        private Boolean isInterrupted = false;
+        private int tasksCount = 0;
+        private int lockedCount = 0;
+
+        public delegate void TaskDelegate();
+        public TaskQueue(int threadsCount) {
+            if (threadsCount < 1)
+                throw new ArgumentOutOfRangeException("Number of threads must be more than 0.");
+
+            this.threadsCount = threadsCount;
+
+            init();
+        }
+
+        private void init()
+        {
+            threads = new Thread[threadsCount];
+
+            for (int i = 0; i < threadsCount; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(Run));
+                threads[i].Start();
+            }
+        }
+
+        private void Run()
+        {
+            while (!isInterrupted)
+            {
+                TaskDelegate task = null;
+                lock (locker)
+                {
+                    if (taskQueue.Count.Equals(0))
+                    {
+                        lockedCount++;
+                        Monitor.Wait(locker);
+                        lockedCount--;
+                    }
+                    else
+                    {
+                        task = taskQueue.Dequeue();
+                        tasksCount++;
+                        Monitor.Pulse(locker);
+                    }
+                }
+
+                if (task != null)
+                {
+                    task();
+                    Interlocked.Decrement(ref tasksCount);
+                }
+
+            }
+
+        }
+
+        public void Interrupt()
+        {
+            while (true)
+            {
+                Thread.Sleep(100);
+                lock (locker)
+                {
+                    if (taskQueue.Count == 0  && tasksCount == 0)
+                        break;
+                }
+            }
+
+            lock (locker)
+            {
+                isInterrupted = true;
+                Monitor.Pulse(locker);
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate task) {
+            lock (locker)
+            {
+                taskQueue.Enqueue(task);
+                if (lockedCount != 0)
+                    Monitor.Pulse(locker);
+            }
+                
+        }
+
+    }
+}
